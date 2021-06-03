a804df6887c52b2a5f3fdf1e39665c79a8053259
Drillin Pierre <sio.drillin@gmail.com>
2021-06-03 14:49:41 +0200

    Adding check postgres extension

--- pgrouting/install/install_1_6.php
+++ pgrouting/install/install_1_6.php
@@ -1,30 +1,48 @@
 <?php
 /**
- * @author    your name
- * @copyright 2011-2020 3liz
- *
- * @see      http://3liz.com
- *
- * @license    All rights reserved
- */
-class pgroutingModuleInstaller extends jInstallerModule
-{
-    public function install()
-    {
+* @package   lizmap
+* @subpackage pgrouting
+* @author    your name
+* @copyright 2011-2020 3liz
+* @link      http://3liz.com
+* @license    All rights reserved
+*/
+
+
+class pgroutingModuleInstaller extends jInstallerModule {
+
+    function preInstall(){
+        // Check if all extensions was install
+        $this->useDbProfile('pgrouting');
+        $db = $this->dbConnection();
+
+        $sql = 'SELECT extname FROM pg_extension WHERE extname = \'postgis\' OR extname = \'pgrouting\';';
+        $result = $db->prepare($sql);
+        $result->execute();
+        $data = $result->fetchall();
+        if (count($data) == 2) {
+            throw new jException('pgrouting~db.query.ext.bad');
+        }
+    }
+
+    function install() {
         // Copy CSS and JS assets
         $this->copyDirectoryContent('../www/css', jApp::wwwPath('assets/pgrouting/css'));
         $this->copyDirectoryContent('../www/js', jApp::wwwPath('assets/pgrouting/js'));
 
         // SQL
         if ($this->firstDbExec()) {
-            $sqlPath = $this->path.'install/sql/install.pgsql.sql';
-            $sqlTpl = jFile::read($sqlPath);
+
+            $this->useDbProfile('pgrouting');
+            $db = $this->dbConnection();
+
+            $sqlPath = $this->path . 'install/sql/install.pgsql.sql';
+            $sqlTpl = jFile::read( $sqlPath );
             $tpl = new jTpl();
             $srid = $this->getParameter('srid');
             $tpl->assign('srid', $srid);
             $sql = $tpl->fetchFromString($sqlTpl, 'text');
-            $this->useDbProfile('pgrouting');
-            $db = $this->dbConnection();
+            
             $db->exec($sql);
         }
     }
