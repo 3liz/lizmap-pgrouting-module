022a99d1b9cc724a4099de0466a809a8a3e67144
Nicolas Boisteault <nboisteault@3liz.com>
2021-06-04 12:23:47 +0200

    Project name must contains 'pgrouting' to enable the module

--- pgrouting/classes/pgroutingDockable.listener.php
+++ pgrouting/classes/pgroutingDockable.listener.php
@@ -3,19 +3,21 @@
     {
         public function onmapDockable($event)
         {
-            $bp = jApp::config()->urlengine['basePath'];
-
-            // dock
-            $content = '<div class="menu-content"><p>Draw origin and destination points.</p></div>';
-            $dock = new lizmapMapDockItem(
-                'pgrouting',
-                'pgRouting',
-                $content,
-                99,
-                $bp.'assets/pgrouting/css/pgrouting.css',
-                $bp.'assets/pgrouting/js/pgrouting.js',
-                ['type' => 'module']
-            );
-            $event->add($dock);
+            // Project name must contains 'pgrouting' to enable the module
+            if (strpos($event->project, 'pgrouting') !== false) {
+                $bp = jApp::config()->urlengine['basePath'];
+                // dock
+                $content = '<div class="menu-content"><p>Draw origin and destination points.</p></div>';
+                $dock = new lizmapMapDockItem(
+                    'pgrouting',
+                    'pgRouting',
+                    $content,
+                    99,
+                    $bp.'assets/pgrouting/css/pgrouting.css',
+                    $bp.'assets/pgrouting/js/pgrouting.js',
+                    ['type' => 'module']
+                );
+                $event->add($dock);
+            }
         }
     }
