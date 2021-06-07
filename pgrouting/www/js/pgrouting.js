365e313fc5f3396422fa7803620fdaaa3bd2c38b
Nicolas Boisteault <nboisteault@3liz.com>
2021-06-07 16:08:35 +0200

    Changement titre de la roadmap

--- pgrouting/www/js/pgrouting.js
+++ pgrouting/www/js/pgrouting.js
@@ -134,7 +134,7 @@ class pgRouting {
                         previousLabel = label;
                     }
 
-                    let roadMap = `<h4>Roadmap</h4><dl>`;
+                    let roadMap = `<h4>Parcours</h4><dl>`;
 
                     for (const road of mergedRoads) {
                         roadMap += `<dt>${road.label}</dt><dd>${road.distance < 1 ? 1 : Math.round(road.distance)}m</dd>`;
@@ -144,7 +144,7 @@ class pgRouting {
                     // Display POI
                     let POIList = '';
                     if (json.poi && json.poi.features) {
-                        POIList += `<h4>POI</h4><dl>`;
+                        POIList += `<h4>Point sur le parcours</h4><dl>`;
                         for (const feature of json.poi.features) {
                             const label = feature.properties.label;
                             const description = feature.properties.description;
