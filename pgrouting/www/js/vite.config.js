4a1003cf38e99b244f390fc705a070a945b36b6d
nboisteault <nboisteault@3liz.com>
2023-03-20 11:06:57 +0100

    vitejs: use different paths for dev and prod - `dist` path is used for prod - the path to js in Lizmap instance is used for dev

--- pgrouting/www/js/vite.config.js
+++ pgrouting/www/js/vite.config.js
@@ -2,17 +2,19 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'pgrouting.js'),
-      // the proper extensions will be added
-      fileName: 'pgrouting',
-      formats: ['es'],
-    },
-    outDir: '../../../tests/lizmap/www/assets/pgrouting/js/',
-    sourcemap: true,
-    emptyOutDir: true,
-  },
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  return {
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, 'pgrouting.js'),
+        // the proper extensions will be added
+        fileName: 'pgrouting',
+        formats: ['es'],
+      },
+      outDir: mode === 'prod' ? 'dist' : '../../../tests/lizmap/www/assets/pgrouting/js/',
+      sourcemap: true,
+      emptyOutDir: true,
+    }
+  }
 })
